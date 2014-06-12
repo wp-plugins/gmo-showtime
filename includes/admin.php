42fc4a7c2bafb103a6c36fd53d053d9babbf7ada
Takeaki Nagashima <Takeaki Nagashima@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-12 01:05:13 +0000

    updated
    git-svn-id: https://plugins.svn.wordpress.org/gmo-showtime/trunk@930720 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin.php
+++ includes/admin.php
@@ -200,11 +200,16 @@ foreach ($this->get_transitions() as $tran) {
 </div><!-- #gmoplugLeft -->
 
 <div id="gmoplugRight">
+<h3>How to Use</h3>
+<ul>
+<li><a href="http://support.wpshop.com/?p=535" target="_blank">How to use the GMO Showtime</a></li>
+</ul>
 <h3>WordPress Themes</h3>
 <ul>
 <li><a href="https://wordpress.org/themes/kotenhanagara" target="_blank">Kotehanagara</a></li>
 <li><a href="https://wordpress.org/themes/madeini" target="_blank">Madeini</a></li>
 <li><a href="https://wordpress.org/themes/azabu-juban" target="_blank">Azabu Juban</a></li>
+<li><a href="http://wordpress.org/themes/de-naani" target="_blank">de naani</a></li>
 </ul>
 <a href="http://wpshop.com/themes?=vn_wps_showtime" target="_blank"><img src="<?php echo ($plugin_file_url.'gmo-showtime/images/'.'wpshop_bnr_themes.png'); ?>" alt="WPShop by GMO WordPress Themes for Everyone!"></a>
 <ul><li class="bnrlink"><a href="http://wpshop.com/themes?=wps_showtime" target="_blank">Visit WP Shop Themes</a></li></ul>
